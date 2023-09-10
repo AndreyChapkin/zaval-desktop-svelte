40411468bd1afc9f0607c9790e4f2b9195062cf4
andrey <test@test>
2023-09-10 17:21:04 +0200

    + Global font

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,3 +3,15 @@
 </script>
 
 <slot />
+
+<style lang="scss">
+	@font-face {
+		font-family: Montserrat;
+		font-weight: normal;
+		src: url('$lib/assets/fonts/Montserrat-Regular.ttf');
+	}
+
+	:global(*) {
+		font-family: Montserrat;
+	}
+</style>
